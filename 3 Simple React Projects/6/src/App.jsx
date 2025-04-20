90da15eea1d143b3869666374749745bb72de82d
Anirudh Raturi <annigeek@gmail.com>
2025-04-20 23:28:08 +0530

    Hidden Search bar

---
+++ 3 Simple React Projects/6/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HiddenSearchBar from './components/HiddenSearchBar'
+import { useState } from 'react'
+
+const App = () => {
+
+  
+  return (
+    <div>
+      <HiddenSearchBar/>
+    </div>
+  )
+}
+
+export default App
+
